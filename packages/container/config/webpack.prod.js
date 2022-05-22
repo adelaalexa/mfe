958e50b442752b70bda3492f6dd7b935ea2a2d0f
alexandraMolnar <alexandra.a.molnar@ibm.com>
2022-05-22 20:47:58 +0100

    Created container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,36 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json')
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+// note mode production takes longer to run
+// however it makes sure we get a more prod ready version out
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+            },
+            shared: packageJson.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+
+}
+
+module.exports = merge(commonConfig, prodConfig);
+
+// filename: '[name].[contenthash].js'
+// makes sure that whenever we create a file for production
+// it will have that as a template for how to name them
+// this is done for caching issues
