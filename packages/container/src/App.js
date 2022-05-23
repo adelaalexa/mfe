6b3ad74a61172e6bcecc5f59b7f3cacaeada4d10
alexandraMolnar <alexandra.a.molnar@ibm.com>
2022-05-23 20:51:59 +0100

    Prod classname fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 // console.log(mount);
 // mount here is a function that takes a reference to an html element
 // because both container and marketing apps are in react it would be a lot
@@ -10,11 +15,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
     return (
+        <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
             <div>
                 <Header />
                 <MarketingApp />
             </div>
         </BrowserRouter>
+        </StylesProvider>
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+// this tells the app that whenever it generates the class names for prod
+// instead of prefixing them with JSS to prefix them with ma
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
 
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing} />
